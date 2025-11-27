722aa57a0800a32b6c49f5e3caa94e77164c44a0
juandirodrib12 <141871531+juandirodrib12@users.noreply.github.com>
2025-11-27 04:34:22 -0500

    arreglo dependencia

--- src/proffesor/proffesor.module.ts
+++ src/proffesor/proffesor.module.ts
@@ -3,12 +3,14 @@ import { ProffesorService } from './proffesor.service';
 import { ProffesorController } from './proffesor.controller';
 import { Proffesor } from './entities/proffesor.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ApiKeyModule } from 'src/api-key/api-key.module';
 
 @Module({
   controllers: [ProffesorController],
   providers: [ProffesorService],
   imports: [
     TypeOrmModule.forFeature([Proffesor]),
+    ApiKeyModule
   ],
   exports: [ProffesorService],
 })
