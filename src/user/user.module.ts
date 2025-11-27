1183f9e4a06513252bd7b334a1320404900fc4aa
juandirodrib12 <141871531+juandirodrib12@users.noreply.github.com>
2025-11-27 05:00:17 -0500

    cambios user

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,12 +4,14 @@ import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ApiKey } from 'src/api-key/entities/api-key.entity';
 import { User } from './entities/user.entity';
+import { ApiKeyModule } from 'src/api-key/api-key.module';
 
 @Module({
   controllers: [UserController],
   providers: [UserService],
   imports: [
-    TypeOrmModule.forFeature([User, ApiKey])
+    TypeOrmModule.forFeature([User, ApiKey]),
+    ApiKeyModule,
   ],
   exports: [UserService],
 })
